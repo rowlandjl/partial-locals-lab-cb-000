581fb083e979facbdfb76f4eb5a3b46e122efe79
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-05 10:56:47 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
   
-  def self.search(arg)
+  def self.search(query)
     self.where("name LIKE ?", arg)
   end 
 end
